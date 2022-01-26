5e270cd13aa0742a1b79fffe714afa425a3c0129
Roman Kolivashko <rkolivashko@gmail.com>
2022-01-26 09:49:46 -0800

    Adding prisma dynamic insert method

--- app/routes/pages/movies.server.ts
+++ app/routes/pages/movies.server.ts
@@ -6,19 +6,38 @@ const prisma = new PrismaClient();
 
 export async function writeToDB() {
   const data = useLoaderData();
-  console.log(data);
+  console.log("this comes from server");
+  const apidata = data.results;
+  console.log(typeof(apidata));
+  
 
-  await prisma.movie.create({
-    data: {
-      title: "Another Test Movie",
-      plot: "Test Plot",
-      // title: data.title,
-      // plot: data.overview,
+  apidata.map((item: any)=> {
+    console.log(item.title);
+    return prisma.movie.create({ 
+      data: {
+        title: item.title,
+        plot: item.overview,
+  
+        //id: id,
+        // title: "New Movie",
+        // plot: "New Plot",
+      },
+    });
 
-      //id: id,
-      // title: "New Movie",
-      // plot: "New Plot",
-    },
-  });
+  })
+  
+
+  // await prisma.movie.create({
+  //   data: {
+  //     title: "Great Movie",
+  //     plot: "Test Plot",
+  //     // title: data.title,
+  //     // plot: data.overview,
+
+  //     //id: id,
+  //     // title: "New Movie",
+  //     // plot: "New Plot",
+  //   },
+  // });
 }
 
