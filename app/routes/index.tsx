533379f2d8ea305721a12a14b82c6260f511d3d1
Elly <eliranmaimon@gmail.com>
2022-01-26 11:45:08 -0800

    add routes index aka homepage

---
+++ app/routes/index.tsx
@@ -0,0 +1,15 @@
+import { Link } from "remix";
+
+export default function getMovies() {
+  return (
+    <div>
+      <h1>Lets pick a movie</h1>
+      <p>Feel free to look around.</p>
+      <ul>
+        <li>
+          <Link to="movies">Begin</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
